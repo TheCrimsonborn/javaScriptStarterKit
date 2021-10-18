c4e757f8731869df7d264c2a85684e03bcfffe39
Halil Acar <xalley47100@gmail.com>
2021-10-19 01:20:07 +0300

    18.10.21

---
+++ src/app.js
@@ -0,0 +1,22 @@
+console.log("Merhaba Kodlama.io");
+
+let dolarDun = 9.10;
+let dolarBugun = 9.20;
+
+const euroBugun = 11;
+console.log(dolarDun);
+console.log(dolarBugun);
+console.log(euroBugun);
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+// console.log("<ul>");
+document.write("<ul>");
+for (let i = 0; i < 3; i++) {
+    // console.log("<li>"+konutKredileri[i]+"</li>");
+    document.write("<li>"+konutKredileri[i]+"</li>");
+    
+}
+document.write("</ul>");
+// console.log("</ul>");
+
